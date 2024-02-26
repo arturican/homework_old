1ce4f28fb47fdc9843c59138d4d974e2d0a0c78a
arturican <arturican@gmail.com>
2024-02-26 18:46:21 +0300

    переписал localCompare

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,19 +4,19 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): any => { // need to fix any
     switch (action.type) {
         case 'sort': {
             if (action.payload === 'up')
-                return [...state.sort((a, b) => a.name > b.name ? 1 : -1)];
+                return [...state].sort((a, b) => a.name.localeCompare(b.name));
             if (action.payload === 'down')
-                return [...state.sort((a, b) => b.name > a.name ? 1 : -1)];
+                return [...state].sort((a, b) => b.name.localeCompare(a.name));
+           break
         }
-            return state
         case 'check': {
-            return state.filter(el => el.age > action.payload) // need to fix
+            return state.filter(el => el.age >= action.payload);
         }
         default:
-            return state
+            return state;
     }
 }
